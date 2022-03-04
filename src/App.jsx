51b6080187a296cc17e9874ccea1be6f267bd193
Andre <andrelebioda@hotmail.de>
2022-03-04 17:07:00 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import '../src/scss/style.scss'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Vite reactjs boilerplate </h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores,
+        necessitatibus sed culpa vero veritatis architecto aliquid laborum ipsum
+        ducimus debitis facere maxime, iure perferendis doloribus minima quia
+        commodi vitae!
+      </p>
+    </div>
+  )
+}
+
+export default App
